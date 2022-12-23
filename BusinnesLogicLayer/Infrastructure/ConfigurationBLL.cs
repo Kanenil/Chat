3b77b862ceec4f40ea03573adf70c0795e3a3d2f
Burd_rw81 <Burd_rw81@student.itstep.org>
2022-12-23 14:08:38 +0200

    Create UserDTO, ConfigurationBLL, IService, UserService

---
+++ BusinnesLogicLayer/Infrastructure/ConfigurationBLL.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLogicLayer.Infrastructure
+{
+    public class ConfigurationBLL
+    {
+        public static void ConfigureServices(ServiceCollection services, string connection)
+        {
+            services.AddDbContext<ChatContext>(opt => opt.UseSqlServer(connection));
+        }
+        public static void AddDependecy(ServiceCollection services)
+        {
+            services.AddSingleton(typeof(DataLayer.Interfaces.IWorkUser), typeof(DataLayer.WorkTemp.WorkUser));
+        }
+    }
+}
